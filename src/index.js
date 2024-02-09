cacc2b210fbdb261817c27862cd1d9e7bdc57f85
ErojiAruna <aruna.eroji@gamil.com>
2024-02-09 18:31:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img src="image/Desk.jpg" alt="aruna" height="300" className="avatar" />
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Eroji Aruna</h1>
+      <p>
+        Front-end web developer. When not coding or preparing a course, I like
+        to read books, to cook (and eat), take care of plants, or just sit and
+        enjoy the sunset.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML+CSS" emoji="💪" color="blue" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="Web Design" emoji="💪" color="#d9f99d" />
+      <Skill skill="Git and GitHub" emoji="👍" color="orangered" />
+      <Skill skill="React" emoji="💪" color="#0ea5e9" />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
